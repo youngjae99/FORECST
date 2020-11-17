ff8d905bde88a2693112745bd9e6e9d60b08b8be
ddiddu <59246296+ddiddu@users.noreply.github.com>
2020-11-17 13:33:33 +0900

    design authentication view

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Input, Space } from 'antd';
+import { Input, Space, Card, Button, Form, Checkbox } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import {Link} from 'react-router-dom';
+import { browserHistory } from 'react-router';
 
 class Authentication extends React.Component{
 
@@ -12,44 +14,146 @@ class Authentication extends React.Component{
             username: "",
             password: ""
         };
+
+        this.handleChange=this.handleChange.bind(this);
+        this.handleLogin=this.handleLogin.bind(this)
+    }
+
+    handleChange(e){
+        let nextState={};
+        nextState[e.target.name]=e.target.value;
+        this.setState(nextState);
+    }
+
+    handleLogin(){
+        let id=this.state.username;
+        let pw=this.state.password;
+
+        this.props.onLogin(id, pw).then(
+            (success)=>{
+                this.setState({
+                    password:''
+                });
+            }
+        )
     }
 
     render(){
+        const layout={
+            labelCol:{
+                span:8,
+            },
+            wrapperCol:{
+                span:16,
+            },
+        };
+
+        const tailLayout={
+            wrapperCol:{
+                offset:8,
+                span:16,
+            },
+        };
+
+        const onFinish=(values)=>{
+            console.log('Success:', values);
+        };
+
+        const onFinishFailed=(errorInfo)=>{
+            console.log('Failed:', errorInfo);
+        };
 
         const inputBoxes = (
-            <Space direction="vertical">
-                <Input placeholder="username"></Input>
-                <Input.Password
-                placeholder="password"
-                iconRender={visible=>(visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}>
-                </Input.Password>
-            </Space>
+            <div>
+                <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required:true,
+                        message: 'Please input your username.'
+                    }
+                ]}>
+                    <Input
+                    name='username'
+                    type='text'
+                    onChange={this.handleChange}
+                    value={this.state.username}>
+                    </Input>
+                </Form.Item>
+
+                <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required:true,
+                        message: 'Please input your password.'
+                    }
+                ]}>
+                    <Input.Password
+                    name='password'
+                    type='password'
+                    onChange={this.handleChange}
+                    value={this.state.password}>
+                    </Input.Password>
+                </Form.Item>
+            </div>
         );
         
         const loginView=(
-            <div>loginView</div>
+            <Form.Item {...tailLayout}>
+                <Button type='primary' htmlType='submit' onClick={this.handleLogin}>
+                    Login
+                </Button>
+            </Form.Item>
         );
 
         const registerView=(
-            <div>registerView</div>
+            <div>
+                <Form.Item {...tailLayout} valuePropName="checked">
+                    <Checkbox>I agree to the Terms & Conditions and Privacy Policy</Checkbox>
+                </Form.Item>
+
+                <Form.Item {...tailLayout} valuePropName="checked">
+                    <Checkbox>I agree to the Email Marketing Policy (optional)</Checkbox>
+                </Form.Item>
+                
+                <Form.Item {...tailLayout}>
+                    <Button type='primary' htmlType='submit'>
+                        Create Account
+                    </Button>
+                </Form.Item>
+            </div>
         );
 
         return(
-            <div>
+            <Card>
                 <div>{this.props.mode ? "LOGIN FORECST" : "JOIN FORECST"}</div>
-                {inputBoxes}
-                <div>{this.props.mode ? loginView : registerView}</div>
-            </div>
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{
+                        remember:true
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}>
+                        {inputBoxes}
+                        {this.props.mode ? loginView : registerView}
+                </Form> 
+            </Card>
         );
     }
 }
 
 Authentication.propTypes={
-    mode: PropTypes.bool
+    mode: PropTypes.bool,
+    onLogin: PropTypes.func,
 };
 
 Authentication.defaultProps={
-    mode: true
+    mode: true,
+    onLogin:(id, pw)=>{console.error("onLogin not defined");},
 };
 
 export default Authentication;
