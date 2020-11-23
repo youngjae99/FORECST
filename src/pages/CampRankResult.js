5b79f47c2def2c94428cb3222d8ec696cf547b14
unknown <jeannechoi@kaist.ac.kr>
2020-11-24 05:27:34 +0900

    slight change in rank

--- src/pages/CampRankResult.js
+++ src/pages/CampRankResult.js
@@ -1,26 +1,42 @@
 import React, {Component} from 'react';
 // import { Menu} from '../components';
-import { Progress, Row, Col, Typography } from 'antd';
+import { Progress, Row, Col, Typography, Alert } from 'antd';
 const { Title } = Typography;
 
 
 class CampRankResult extends React.Component {
     render(){
         return (
-            <div style={{padding:20}}>
-                {/* <Menu></Menu> */}
+            <div style={{padding:20, fontFamily: "Roboto"}}>
+                <Alert
+                    message="Your choice is successfully count in the result!"
+                    type="success"
+                    showIcon
+                    closable
+                    banner
+                />
                 <Title>Results for the Final Ranking</Title>
                 <Title level={3}>'Project D' is the winner. Congratulations!</Title>
-                <Progress percent={2} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project A
                 <Progress percent={8} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project B
                 <Progress percent={15} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project C
                 <Progress percent={5} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project D
                 <Progress percent={25} status="active" strokeColor={'#1890ff'} strokeWidth={'15px'}></Progress>
+                Project E
                 <Progress percent={10} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project F
                 <Progress percent={12} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project G
                 <Progress percent={13} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project H
                 <Progress percent={6} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project I
                 <Progress percent={4} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
+                Project J
+                <Progress percent={2} strokeColor={'#99ceff'} strokeWidth={'15px'}></Progress>
             </div>
         )
     }
