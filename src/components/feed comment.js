3cce9fbc4768e7d36c234d5bf746b827c9fb7f75
ddiddu <59246296+ddiddu@users.noreply.github.com>
2020-12-06 03:46:47 +0900

    modify modal comment

--- src/components/feed comment.js
+++ src/components/feed comment.js
@@ -172,11 +172,12 @@ class FeedComment extends Component {
 
   modal_water = ()=>{
     const modal = Modal.info({
-    title: 'You watered 2 points to '+this.props.id+"!",
+    title: 'Your watering grow '+this.props.id+"'s tree 2 point!",
     content: (
-      <img src={popup_water} alt="wc" style={{ width: "300px", height: "300px" }}/>
+      <img src={popup_water} alt="wc" style={{ width: "300px"}}/>
     ),
-    okButtonProps: {style: {display: "none"}}
+    okButtonProps: {style: {display: "none"}},
+    centered: true
     
   });
   setTimeout(() => {
