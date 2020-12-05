33639b450117aa86577d37691f5b85ec2ea0a946
youngjae <elite1111cyj@gmail.com>
2020-12-06 01:52:38 +0900

    Main- join hackathon page renewal

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { Button, Typography } from "antd";
+import { Button, Typography, Card, Col, Row} from "antd";
 import "./MainPage.css";
 
 import "./template/css/styles.css";
@@ -53,9 +53,9 @@ class MainPage extends React.Component {
                       finish the project, and freely share your coding
                       experiences.
                     </p>
-                    <a className="btn-solid-lg page-scroll" href="/explore">
+                    <Link className="btn-solid-lg page-scroll" to="/explore">
                       EXPLORE
-                    </a>
+                    </Link>
                   </div>
                 </div>
                 <div className="col-lg-6">
@@ -75,6 +75,8 @@ class MainPage extends React.Component {
     );
 
     const goCampPage = (
+
+      /*
       <div className="header-content">
         <div className="container">
           <div
@@ -158,9 +160,9 @@ class MainPage extends React.Component {
                     4. Design a service that helps children to concentrate
                     easily on their studying! (11/21~12/8)
                     <Button type="primary" style={{ marginLeft: "16px" }}>
-                      {/* <Link to={"/camp"} style={{ fontSize: 18 }}> */}
+                      {}
                       <div style={{ fontSize: 18 }}>Enter Hackathon</div>
-                      {/* </Link> */}
+                      {}
                     </Button>
                   </Title>
                 </div>
@@ -168,6 +170,73 @@ class MainPage extends React.Component {
             </div>
           </div>
         </div>
+      </div>
+      */
+     <div className="banner" style={{ fontFamily: "Roboto" }}>
+        <link
+          href="https://fonts.googleapis.com/css?family=Raleway:400,400i,600,700,700i&amp;subset=latin-ext"
+          rel="stylesheet"
+        ></link>
+        <header id="header" className="header">
+          <div className="header-content">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="text-container">
+                    <h1>
+                      Join the hackathon right now
+                    </h1>
+                    <p className="p-large">
+                      There are many hackathons waiting for you here.
+                    </p>
+                    <Link className="btn-solid-lg page-scroll" to="/explore">
+                      Enter Hackathon
+                    </Link>
+                  </div>
+                  
+                </div>
+                
+              </div>
+
+              <div className="site-card-wrapper" style={{marginTop:"40px"}}>
+                <Row className="camp-row" gutter={16}>
+                  <Col span={8}>
+                  <Card title="12/3 ~ 12/10" extra={<Button danger><Link to={"/camp"} className="nowOn">ENTER NOW</Link></Button>} style={{ width: 300 }}>
+                    <h6>Make an application for the pandemic COVID 19 situation!
+                    (12/3 ~ 12/10)</h6>
+                    <p style={{color:"red", fontWeight:"bold"}}>Now on going!</p>
+                  </Card>
+                  </Col>
+                  <Col span={8}>
+                  <Card title="12/11~" extra={<Button danger disabled><Link to={"/camp"} className="nowOn">ENTER NOW</Link></Button>} style={{ width: 300 }}>
+                    <h6>Create a website using React and Firebase. (Starting from
+                    12/11)</h6>
+                  </Card>
+                  </Col>
+                  <Col span={8}>
+                  <Card title="12/25 ~" extra={<Button danger disabled><Link to={"/camp"} className="nowOn">ENTER NOW</Link></Button>} style={{ width: 300 }}>
+                    <h6>Come up with a platform for people who live alone in the
+                    city. (Starting from 12/25)</h6>
+                  </Card>
+                  </Col>
+                </Row>
+
+                <Row className="camp-row" gutter={16}>
+                  <Col span={8}>
+                  <Card title="12/26 ~ " extra={<Button danger disabled><Link to={"/camp"} className="nowOn">ENTER NOW</Link></Button>} style={{ width: 300 }}>
+                    <h6>Design a service that helps children to concentrate
+                    easily on their studying! (12/26)</h6>
+                  </Card>
+                  </Col>
+                </Row>
+              </div>
+              
+            </div>
+            
+          </div>
+          
+        </header>
+        
       </div>
     );
 
