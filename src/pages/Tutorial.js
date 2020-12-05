750eb5057366d0fb3970932febac2014bed406b6
ddiddu <59246296+ddiddu@users.noreply.github.com>
2020-12-06 02:35:08 +0900

    add tutorial

---
+++ src/pages/Tutorial.js
@@ -0,0 +1,119 @@
+import React, {useState} from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button, Typography, Modal } from "antd";
+import "./MainPage.css";
+
+import "./template/css/styles.css";
+import "./template/css/swiper.css";
+import "./template/css/magnific-popup.css";
+import "./template/css/bootstrap.css";
+import "./template/css/fontawesome-all.css";
+import mainimage from "./template/images/camp1.png";
+
+const { Title } = Typography;
+
+const ReachableContext = React.createContext();
+const UnreachableContext = React.createContext();
+
+const config = {
+  title: 'Use Hook!',
+  content: (
+    <>
+      <ReachableContext.Consumer>{name => `Reachable: ${name}!`}</ReachableContext.Consumer>
+      <br />
+      <UnreachableContext.Consumer>{name => `Unreachable: ${name}!`}</UnreachableContext.Consumer>
+    </>
+  ),
+};
+
+// const [modal, contextHolder] = Modal.useModal();
+
+class Tutorial extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={};
+  }
+
+  handleOk=()=>{
+    this.props.history.push(`/mypage/${this.props.currentUser}`);
+    Modal.destroyAll();
+  }
+
+  handleStart=()=>{
+    Modal.info({
+        title: "Now, we are going to start by writing a to-do list.",
+        content: (
+            <div>
+                Your first to-do list must be 'Making a project name'
+                <div style={{float: "right", marginTop: 20}}>
+                    <Button type="primary" onClick={this.handleOk}>
+                        {/* <Link to={{pathname: `/mypage/${this.props.currentUser}`}} style={{fontSize: 18}}>START</Link>} */}
+                        START
+                    </Button>
+                </div>
+            </div>  
+        ),
+        width: 500,
+        centered: true,
+        okButtonProps: {style: {display: "none"}},
+        onCancel(){}
+      });
+  }
+
+  render() {
+    const joinPage = (
+      <div className="banner" style={{ fontFamily: "Roboto" }}>
+        <link
+          href="https://fonts.googleapis.com/css?family=Raleway:400,400i,600,700,700i&amp;subset=latin-ext"
+          rel="stylesheet"
+        ></link>
+        <header id="header" className="header">
+          <div className="header-content">
+            <div className="container">
+              <div className="row">
+                  <div className="text-container">
+                    <h1 style={{fontSize: 35}}>
+                      <span className="primaryColor" style={{fontSize: 50, color: "#482482", fontWeight: "bold"}}> In this Hackathon, </span>
+                      <br></br> You can write a to-do list and post a feed if you complete it,
+                      <br></br> Ask questions freely,
+                      <br></br> And submit your project and vote for the final ranking.
+                      <br></br> 
+                      <span className="primaryColor" style={{fontSize: 35, color: "#482482"}}>You can grow your tree by participating actively.</span>
+                    </h1>
+                    <p className="p-large">
+                      You will get a prize either if you win final ranking, or if your tree level is highest.
+                      <br></br> Now, let's start by writing a to-do list, and do first posting.
+                    </p>
+                    <a className="btn-solid-lg page-scroll"
+                    style={{backgroundColor: "#482482", borderColor: "#482482", color: "white"}}
+                    onClick={this.handleStart}>
+                        START
+                    </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+
+    return (
+      <div style={{ fontFamily: "Roboto", paddingTop: "10px" }}>
+        {joinPage}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    status: state.authentication.status,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tutorial);
